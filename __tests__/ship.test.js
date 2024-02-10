9457a6a3aeb6e74a65b4a461ac9ec0f9d5f307be
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-02-10 15:24:31 -0800

    Completed ship factory function and corresponding methods + tests.

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,12 +1,36 @@
-const createShip = require("../src/ship");
+const { createShip } = require("../src/ship");
 
 /*
 TODO Your ‘ships’ will be objects that include:
-TODO 1/ their length, 
+// TODO 1/ their length, 
 TODO 2/ the number of times they’ve been hit and 
 TODO 3/ whether or not they’ve been sunk.
 */
 
 describe("Ship", () => {
-  test("has a length", () => {});
+  test("has a length", () => {
+    const ship = createShip(4);
+    expect(ship.length).toBe(4);
+  });
+
+  test("tracks the number of times it has been hit", () => {
+    const ship = createShip(3);
+    ship.hit();
+    ship.hit();
+    expect(ship.getHits()).toBe(2);
+  });
+
+  test("is not sunk if hits are less than length", () => {
+    const ship = createShip(3);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test("is sunk if hits are equal to length", () => {
+    const ship = createShip(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });
--- src/ship.js
+++ src/ship.js
@@ -4,12 +4,19 @@ export default function ship() {
 }
 */
 
-export const createShip = () => {
+export const createShip = (length) => {
+  let hits = 0;
   const call = () => {
     console.log("I get called from ship.js!");
   };
 
   return {
+    length,
     call,
+    hit: () => {
+      hits += 1;
+    },
+    getHits: () => hits,
+    isSunk: () => hits === length,
   };
 };
