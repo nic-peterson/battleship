836da2f59667b36b2f6081d19f0d2412e9b5166c
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-12-22 20:01:50 -0800

    Remove redundant files.

--- src/helpers/cell.js
+++
@@ -1,8 +0,0 @@
-// src/cell.js
-import { CellStatus } from "./constants.js";
-
-export const createCell = () => ({
-  ship: null, // Reference to the ship object; null if no ship is placed
-  isHit: false, // Indicates if the cell has been attacked
-  status: CellStatus.EMPTY, // Uses the enumeration for status
-});
